Havoc-OS/android_frameworks_base

CompassTile: Add a device supports compass check
- Previously it showed up on devices even without compass support
  making it a bloat

Change-Id: I79d37b90255e0d674e73e45452a510c8d990e9dd
Signed-off-by: PMS22 <prathams99@rediff.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -28,6 +28,7 @@ import android.widget.ImageView;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.havoc.HavocUtils;
 import com.android.systemui.R;
 import com.android.systemui.plugins.qs.QSIconView;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -74,6 +75,11 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
         QSIconView iconView = super.createTileView(context);
         mImage = (ImageView) iconView.findViewById(android.R.id.icon);
         return iconView;
+    }
+	
+	@Override
+    public boolean isAvailable() {
+        return HavocUtils.deviceHasCompass(mContext);
     }
 
     @Override

mracar07/platform_frameworks_base

Change-Id: Ief8908c412d3e5e8dbcd40998b9343ff68d8783c
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Signed-off-by: mracar07 <umutcan@umutcanacar.me>

packages/SystemUI/src/com/android/systemui/qs/tiles/CompassTile.java
@@ -28,6 +28,7 @@ import android.widget.ImageView;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.aosap.aosapUtils;
 import com.android.systemui.R;
 import com.android.systemui.plugins.qs.QSIconView;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
@@ -161,6 +162,11 @@ public class CompassTile extends QSTileImpl<BooleanState> implements SensorEvent
         } else {
             return mContext.getString(R.string.accessibility_quick_settings_compass_off);
         }
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return aosapUtils.deviceHasCompass(mContext);
     }
 
     @Override