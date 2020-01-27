b9f60b4aa04c40e3551520e92d6145eb2e521aa7
00day0 <therandomuser11@gmail.com>
2020-01-28 00:07:58 +0100

    FODCircleView: Recognizing fingerprint animation [1/2]
    Change-Id: I86a49d2d8f7607d0f81b84e7e751930309ec4a1f
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5880,6 +5880,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
 
         /**
+         * FOD recognizing animation
+         * @hide
+         */
+        public static final String FOD_RECOGNIZING_ANIMATION = "fod_recognizing_animation";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
